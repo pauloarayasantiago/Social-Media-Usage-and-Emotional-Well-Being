ee29bd647083b43a6e4ac8d43167b01b2f1e7fab
pauloarayasantiago <166965210+pauloarayasantiago@users.noreply.github.com>
2024-07-21 17:28:23 -0600

    update

---
+++ run_voila.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Navigate to the directory containing your Jupyter notebook
+cd "C:/Users/paulo/OneDrive/Documents/Social-Media-Usage-and-Emotional-Well-Being"
+
+# Run Voilà with your notebook
+voila "voila practice.ipynb"
